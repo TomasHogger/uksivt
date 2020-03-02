07fb1c87cd05cfa4d6dad6150493064b1372c8e0
Tomas_Hogger <avdeev_artem.2001@inbox.ru>
2020-03-02 14:03:15 +0500

    fix

--- src/main/java/com/example/uksivtTest/UksivtTest/repository/StudentRepository.java
+++ src/main/java/com/example/uksivtTest/UksivtTest/repository/StudentRepository.java
@@ -1,6 +1,7 @@
 package com.example.uksivtTest.UksivtTest.repository;
 
 import com.example.uksivtTest.UksivtTest.model.Student;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
@@ -11,6 +12,7 @@ public interface StudentRepository extends CrudRepository<Student, Integer> {
 
     Student findByLogin(String login);
 
+    @Query(value = "SELECT * FROM student WHERE login LIKE %?1%", nativeQuery = true)
     List<Student> findByLoginLike(String login);
 
 }
